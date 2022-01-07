12e4e90e5b4a2bbeab440fbdefbb09ef1242ed43
kasaderos <kberda99@gmail.com>
2022-01-07 19:38:52 +0600

    add recursive call

--- instruction.go
+++ instruction.go
@@ -21,6 +21,33 @@ func (f *function) isTerminal() bool {
 	return f.terminal != nil
 }
 
+func back(stack *Stack, ret bool) {
+	for !stack.Empty() {
+		f := stack.Top().f
+		i := stack.Top().i
+		switch f.typ {
+		case "T":
+			stack.Pop()
+		case "L":
+			if ret {
+				if i+1 < len(f.funcs) {
+					stack.Pop()
+					stack.Push(Frame{f, i + 1})
+					stack.Push(Frame{f.funcs[i+1], 0})
+					return
+				}
+			}
+			stack.Pop()
+		case "N":
+			stack.Pop()
+			if !ret && i+1 < len(f.funcs) {
+				stack.Push(Frame{f.funcs[i+1], i + 1})
+				return
+			}
+		}
+	}
+}
+
 func execute(f function) bool {
 	stack := &Stack{}
 	stack.Push(Frame{f, 0})
@@ -31,36 +58,18 @@ func execute(f function) bool {
 		f := stack.Top().f
 		i := stack.Top().i
 		switch f.typ {
-		case "N":
+		case "N", "L":
 			// if non terminal is not empty push
-			if len(f.funcs) > 0 {
-				stack.Push(Frame{f.funcs[0], 0})
+			if i < len(f.funcs) {
+				stack.Push(Frame{f.funcs[i], i})
+			} else {
+				back(stack, ret)
 			}
 		case "T":
 			ret = f.call()
-			for !stack.Empty() {
-				f = stack.Top().f
-				i = stack.Top().i
-				if ret && (f.typ == "C" || f.typ == "L") {
-					break
-				}
-				if f.typ == "N" && i+1 < len(f.funcs) {
-					stack.Pop()
-					stack.Push(Frame{f.funcs[i+1], i + 1})
-					break
-				}
-				stack.Pop()
-			}
-		case "L":
-			if ret && i+1 < len(f.funcs) {
-				ret = false
-				stack.Pop()
-				stack.Push(Frame{f, i + 1})
-				stack.Push(Frame{f.funcs[i+1], i + 1})
-			} else {
-				stack.Push(Frame{f.funcs[0], 0})
-			}
-			// case "C":
+			back(stack, ret)
+		default:
+			back(stack, ret)
 		}
 	}
 	return ret
--- instruction_test.go
+++ instruction_test.go
@@ -5,84 +5,131 @@ import (
 	"testing"
 )
 
+func assert(t *testing.T, b bool) {
+	if !b {
+		t.Errorf("assert failed\n")
+	}
+}
+
 func TestExecuteSimple(t *testing.T) {
+	n := 0
 	f := function{
 		typ: "N",
 		funcs: []function{
-			{typ: "T", terminal: func() bool { fmt.Println("A"); return false }},
-			{typ: "T", terminal: func() bool { fmt.Println("B"); return false }},
+			{typ: "T", terminal: func() bool { n++; return false }},
+			{typ: "T", terminal: func() bool { n++; return false }},
 		},
 	}
 	execute(f)
+	assert(t, n == 2)
 }
 
-func TestExecuteComplex(t *testing.T) {
+func TestExecuteComplex1(t *testing.T) {
+	n := 0
 	f := function{
 		typ: "N",
 		funcs: []function{
 			{typ: "N", funcs: []function{
 				{typ: "N", funcs: []function{
-					{typ: "T", terminal: func() bool { fmt.Println("C"); return false }},
+					{typ: "T", terminal: func() bool { n++; return false }},
 				}},
 			},
 			},
-			{typ: "T", terminal: func() bool { fmt.Println("D"); return false }},
+			{typ: "T", terminal: func() bool { n++; return false }},
 		},
 	}
 	execute(f)
+	assert(t, n == 2)
 }
 
-func TestExecuteLogic1(t *testing.T) {
-	f := function{typ: "N"}
-	f.funcs = append(f.funcs, function{
-		typ: "L",
-		funcs: []function{
-			{typ: "T", terminal: func() bool { fmt.Println("A"); return false }},
-			{typ: "T", terminal: func() bool { fmt.Println("B"); return false }},
-		},
-	},
-	)
-	fmt.Println(execute(f))
-}
-
-func TestExecuteLogic2(t *testing.T) {
-	f := function{typ: "N"}
-	f.funcs = append(f.funcs, function{
-		typ: "L",
-		funcs: []function{
-			{typ: "T", terminal: func() bool { fmt.Println("A"); return true }},
-			{typ: "T", terminal: func() bool { fmt.Println("B"); return false }},
-		},
-	},
-	)
-	fmt.Println(execute(f))
-}
-
-func TestExecuteLogic3(t *testing.T) {
-	f := function{typ: "N"}
-	f.funcs = append(f.funcs, function{
-		typ: "L",
-		funcs: []function{
-			{typ: "T", terminal: func() bool { fmt.Println("A"); return true }},
-			{typ: "T", terminal: func() bool { fmt.Println("B"); return true }},
-			{typ: "T", terminal: func() bool { fmt.Println("C"); return false }},
-		},
-	},
-	)
-	fmt.Println(execute(f))
-}
-
-func TestExecuteLogic4(t *testing.T) {
+func TestExecuteComplex2(t *testing.T) {
+	n := 0
 	f := function{typ: "N"}
 	f2 := function{typ: "N"}
 	f2.funcs = append(f2.funcs, function{
-		typ: "L",
+		typ: "N",
 		funcs: []function{
-			{typ: "T", terminal: func() bool { fmt.Println("A"); return true }},
-			{typ: "T", terminal: func() bool { fmt.Println("B"); return false }},
+			{typ: "N"},
+			{typ: "N"},
 		},
 	},
 	)
+	f2.funcs = append(f2.funcs, function{typ: "T", terminal: func() bool { n++; return false }})
 	f.funcs = append(f.funcs, f2)
 	fmt.Println(execute(f))
+	assert(t, n == 1)
+}
+
+func TestExecuteLogic1(t *testing.T) {
+	n := 0
+	f := function{typ: "N"}
+	f.funcs = append(f.funcs, function{
+		typ: "L",
+		funcs: []function{
+			{typ: "T", terminal: func() bool { n++; return false }},
+			{typ: "T", terminal: func() bool { n++; return false }},
+		},
+	},
+	)
+	fmt.Println(execute(f))
+	assert(t, n == 1)
+}
+
+func TestExecuteLogic2(t *testing.T) {
+	n := 0
+	f := function{typ: "N"}
+	f.funcs = append(f.funcs, function{
+		typ: "L",
+		funcs: []function{
+			{typ: "T", terminal: func() bool { n++; return true }},
+			{typ: "T", terminal: func() bool { n++; return false }},
+		},
+	},
+	)
+	fmt.Println(execute(f))
+	assert(t, n == 2)
+}
+
+func TestExecuteLogic3(t *testing.T) {
+	n := 0
+	f := function{typ: "N"}
+	f.funcs = append(f.funcs, function{
+		typ: "L",
+		funcs: []function{
+			{typ: "T", terminal: func() bool { n++; return true }},
+			{typ: "T", terminal: func() bool { n++; return true }},
+			{typ: "T", terminal: func() bool { n++; return false }},
+		},
+	},
+	)
+	fmt.Println(execute(f))
+	assert(t, n == 3)
+}
+
+func TestExecuteLogic4(t *testing.T) {
+	n := 0
+	f := function{typ: "N"}
+	f2 := function{typ: "L"}
+	f2.funcs = append(f2.funcs, function{
+		typ: "L",
+		funcs: []function{
+			{typ: "T", terminal: func() bool { n++; return true }},
+			{typ: "T", terminal: func() bool { n++; return true }},
+		},
+	},
+	)
+	f2.funcs = append(f2.funcs, function{typ: "T", terminal: func() bool { n++; return false }})
+	f.funcs = append(f.funcs, f2)
+	fmt.Println(execute(f))
+	assert(t, n == 3)
+}
+
+func TestExecuteRecursive(t *testing.T) {
+	// F = T | F
+	n := 0
+	f := function{typ: "L"}
+	f.funcs = append(f.funcs, function{typ: "T", terminal: func() bool { n++; return n < 3 }})
+	f.funcs = append(f.funcs, f)
+	execute(f)
+	assert(t, n == 3)
 }
